8567b502872d1ba09b7170e9cc36396fd426c39f
DAYARAM99 <56449984+DAYARAM99@users.noreply.github.com>
2019-10-11 22:48:29 +0530

    Update colourspaces.py

--- colourspaces.py
+++ colourspaces.py
@@ -17,7 +17,8 @@ img2 = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 cv2.imshow("bgr image",img2)                                    ## original image               ## Convert BGR to RGB image
 
 #printing hsv image of an image
-hsv_image = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)                   ## Convert BGR to HSV image 
+hsv_image = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)  
+Rgb_img=cv1.cvtColor(img,COLOR_BGR2RGB)                 ## Convert BGR to HSV image 
 cv2.imshow("hsv image",hsv_image)
 
 cv2.waitKey(0)
